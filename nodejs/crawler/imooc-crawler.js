59d4fcf568b6a3cd99bf7dda1e12155b92591eef
Sugary <guo.yang@hypers.com>
2015-08-10 19:02:47 +0800

    新增了nodejs文件操作及刷钱脚本

--- nodejs/crawler/imooc-crawler.js
+++ nodejs/crawler/imooc-crawler.js
@@ -2,8 +2,15 @@
  * Created by hypers-godfery on 2015/7/10.
  */
 var http = require("http");
-var url = "http://www.imooc.com/learn/282";
+var fs = require('fs');
+var iconv = require('iconv-lite');
 var cheerio = require("cheerio");
+
+
+var url = "http://www.imooc.com/learn/282",
+    file = "D:\\a.txt";
+
+var txt = "";
 
 
 function filterChapters(html) {
@@ -49,10 +56,12 @@ function printCousrDataInfo(couseData){
     couseData.forEach(function(item){
        var chapterTitle = item.chapterTitle;
 
-        console.log(chapterTitle + "\n");
+        txt += chapterTitle + "\r\n";
+        //console.log(chapterTitle + "\n");
 
         item.videos.forEach(function(video){
-           console.log(" 【" + video.id + "】" + video.title + "\n");
+            txt += " 【" + video.id + "】" + video.title + "\r\n";
+           //console.log(" 【" + video.id + "】" + video.title + "\n");
         });
     });
 }
@@ -68,7 +77,26 @@ http.get(url, function (res) {
         //console.log(html)
         var courseData= filterChapters(html);
         printCousrDataInfo(courseData);
+        writeFile(file);
     });
 }).on("error", function () {
     console.log("错误");
 });
+
+
+function writeFile(file){
+    // 测试用的中文
+    //var str = "\r\n我是一个人Hello myself!";
+    // 把中文转换成字节数组
+    var arr = iconv.encode(txt, 'gbk');
+    //console.log(arr);
+
+    // appendFile，如果文件不存在，会自动创建新文件
+    // 如果用writeFile，那么会删除旧文件，直接写新文件
+    fs.appendFile(file, arr, function(err){
+        if(err)
+            console.log("fail " + err);
+        else
+            console.log("写入文件ok");
+    });
+}
